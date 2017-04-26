a77fe18c130d5560aa8f44986797f177ffdd6c87
Yasuyuki Kamata <yasuyuki@unity3d.com>
2017-04-25 17:08:53 -0700

    First commit

---
+++ Assets/CodelessAds/Editor/CodelessAdsEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEditor.Events;
+
+public class CodelessAdsEditor : MonoBehaviour
+{
+	[MenuItem("Unity Ads/Create Unity Ads Button")]
+	static void CreateAdsButton()
+	{
+		// Create Button
+		EditorApplication.ExecuteMenuItem("GameObject/UI/Button");
+
+		// Get GameObject of Button
+		GameObject gameObject = Selection.activeGameObject;
+		CodelessAds codelessAds = gameObject.AddComponent<CodelessAds> ();
+		Button button = gameObject.GetComponent<Button> ();
+//		button.onClick.AddListener (codelessAds.ShowAds);
+		UnityEventTools.AddPersistentListener (button.onClick, codelessAds.ShowAds);
+		Text text = button.GetComponentInChildren<Text> ();
+		text.text = "Show Unity Ads";
+	}
+}
---
+++ Assets/CodelessAds/Scripts/CodelessAds.cs
@@ -0,0 +1,49 @@
+﻿#if UNITY_ADS
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.Events;
+
+public class CodelessAds : MonoBehaviour
+{
+	[SerializeField]
+	UnityEvent OnFinishedCallcack;
+	[SerializeField]
+	UnityEvent OnSkippedCallback;
+	[SerializeField]
+	UnityEvent OnFailedCallback;
+
+	void Start ()
+	{
+		ShowOptions options = new ShowOptions();
+		options.resultCallback = HandleShowResult;
+	}
+
+	public void ShowAds ()
+	{
+		if (Advertisement.IsReady ()) {
+			Advertisement.Show ();
+		}
+	}
+
+	private void HandleShowResult (ShowResult result)
+	{
+		switch (result)
+		{
+		case ShowResult.Finished:
+			Debug.Log ("Video completed.");
+			OnFinishedCallcack.Invoke ();
+			break;
+		case ShowResult.Skipped:
+			Debug.LogWarning ("Video was skipped.");
+			OnSkippedCallback.Invoke ();
+			break;
+		case ShowResult.Failed:
+			Debug.LogError ("Video failed to show.");
+			OnFailedCallback.Invoke ();
+			break;
+		}
+	}
+}
+#endif
